659285c70168790388d3d4e172f491d5cbd9b56b
Alex Paz <alexjpaz@gmail.com>
2015-01-29 16:46:51 -0500

    test.js

--- app/test.js
+++ app/test.js
@@ -1,8 +1,16 @@
 var webPage = require('webpage');
+var server = require('webserver').create();
+
 var page = webPage.create();
 
 page.viewportSize = { width: 1920, height: 1080 };
+
 page.open("http://www.google.com", function start(status) {
-    page.render('google_home.jpeg', {format: 'jpeg', quality: '100'});
-  phantom.exit();
+});
+
+server.listen(8080, function(request, response) {
+  page.render('google_home.jpeg', {format: 'jpeg', quality: '100'});
+
+  response.statusCode = 200;
+  response.write("created") //TODO write out string from render
 });
